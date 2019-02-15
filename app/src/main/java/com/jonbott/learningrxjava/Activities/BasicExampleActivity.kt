997f2b378f874f4c4c670f4bd59d8c433089fee1
taracha <rojtaracha@gmail.com>
2019-02-15 13:07:39 +0300

    Simple Networking:
    [+] Add CompositeDisposable Bag
    
    [+] Create a Retrofit Service that we will consume.

--- app/src/main/java/com/jonbott/learningrxjava/Activities/BasicExampleActivity.kt
+++ app/src/main/java/com/jonbott/learningrxjava/Activities/BasicExampleActivity.kt
@@ -2,10 +2,40 @@ package com.jonbott.learningrxjava.Activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.jonbott.learningrxjava.ModelLayer.Entities.Posting
 import com.jonbott.learningrxjava.R
+import io.reactivex.disposables.CompositeDisposable
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
 
 
 class BasicExampleActivity : AppCompatActivity() {
+
+    // CompositeDisposable Bag
+    private var bag = CompositeDisposable()
+
+    // region Simple Network Layer
+
+    // Retrofit interface
+    interface JsonPlaceHolderService {
+        @GET("posts/{id}")
+        fun getPosts(@Path("id") id: String): Call<Posting>
+    }
+
+    // Instance of Retrofit
+    private var retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://jsonplaceholder.typicode.com/")
+            .build()
+
+    // Actual service that we will consume
+    private var service = retrofit.create(JsonPlaceHolderService::class.java)
+
+
+    // endregion
 
 
     //region Life Cycle Events
