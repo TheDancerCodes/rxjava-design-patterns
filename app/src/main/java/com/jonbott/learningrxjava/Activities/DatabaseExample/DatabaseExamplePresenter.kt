71d87ebf49197d5c029deca81e495ad06ce2aa93
taracha <rojtaracha@gmail.com>
2019-02-19 09:45:04 +0300

    Database: Model layer to UI
    [+] Consume Model changes in the ViewAdapter for the list.
    
    [+] Consume the above changes in the Presenter.

--- app/src/main/java/com/jonbott/learningrxjava/Activities/DatabaseExample/DatabaseExamplePresenter.kt
+++ app/src/main/java/com/jonbott/learningrxjava/Activities/DatabaseExample/DatabaseExamplePresenter.kt
@@ -1,7 +1,25 @@
 package com.jonbott.learningrxjava.Activities.DatabaseExample
 
+import com.jakewharton.rxrelay2.BehaviorRelay
 import com.jonbott.learningrxjava.ModelLayer.ModelLayer
+import com.jonbott.learningrxjava.ModelLayer.PersistenceLayer.PhotoDescription
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
 
 class DatabaseExamplePresenter {
     val modelLayer = ModelLayer.shared //normally injected
+
+    // Consume changes in the presenter
+    val photoDescriptions: BehaviorRelay<List<PhotoDescription>>
+        // Use a getter & return the model layers description of this
+        get() = modelLayer.photoDescriptions // Bubbling up for the lower layers
+
+    init {
+        launch {
+            delay(3000) // # seconds delay
+
+            // Model layer loads all PhotoDescriptions from DB
+            modelLayer.loadAllPhotoDescriptions()
+        }
+    }
 }
